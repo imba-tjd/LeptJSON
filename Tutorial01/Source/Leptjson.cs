de6699b73aad6a0b72f50c62df884ad202eaf772
imba-tjd <109224573@qq.com>
2018-09-02 17:38:59 +0800

    Update Tutorial01: Fix English grammar

--- Tutorial01/Source/Leptjson.cs
+++ Tutorial01/Source/Leptjson.cs
@@ -69,13 +69,13 @@ namespace Tutorial01.Leptjson
 
             return LeptParseResult.OK;
         }
-        LeptParseResult ParseLiteral(LeptContext context, string literal, LeptType expectType)
+        LeptParseResult ParseLiteral(LeptContext context, string literal, LeptType expectedType)
         {
             if (!context.json.StartsWith(literal))
                 return LeptParseResult.InvalidValue;
 
             context.json = context.json.Substring(literal.Length);
-            Type = expectType;
+            Type = expectedType;
 
             return LeptParseResult.OK;
         }
