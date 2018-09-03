0e30289ae5334986b9f6cda1b4781a94880b20d8
imba-tjd <109224573@qq.com>
2018-09-03 11:30:46 +0800

    Update Tutorial01: Fix test case

--- Tutorial01/UnitTest/UnitTest.cs
+++ Tutorial01/UnitTest/UnitTest.cs
@@ -11,18 +11,21 @@ namespace Tutorial01.UnitTest
         [Fact]
         public void TestParseRootNotSingular()
         {
+            parser.GetType().GetProperty("Type").SetValue(parser, LeptType.False);
             Assert.Equal(LeptParseResult.RootNotSingular, parser.Parse("null x"));
             Assert.Equal(LeptType.Null, parser.Type);
         }
         [Fact]
         public void TestParseInvalidValue()
         {
+            parser.GetType().GetProperty("Type").SetValue(parser, LeptType.False);
             Assert.Equal(LeptParseResult.InvalidValue, parser.Parse("nul"));
             Assert.Equal(LeptType.Null, parser.Type);
         }
         [Fact]
         public void TestExpectValue()
         {
+            parser.GetType().GetProperty("Type").SetValue(parser, LeptType.False);
             Assert.Equal(LeptParseResult.ExpectValue, parser.Parse(""));
             Assert.Equal(LeptType.Null, parser.Type);
 
@@ -33,8 +36,9 @@ namespace Tutorial01.UnitTest
         [Fact]
         public void TestNull()
         {
+            parser.GetType().GetProperty("Type").SetValue(parser, LeptType.False);
             Assert.Equal(LeptParseResult.OK, parser.Parse("null"));
-            Assert.Equal(LeptType.Null, parser.Type); // This will always success because Type was set to Null even when parsing failed.
+            Assert.Equal(LeptType.Null, parser.Type);
         }
         [Fact]
         public void TestTrue()
